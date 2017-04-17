5f58155929910928f106c1bfee469bdf94a3ba5b
Rudolph Layko <rudy_layko@Rudolphs-MacBook-Pro.local>
2017-04-17 20:50:30 +0300

    get w2v models with diff dim

---
+++ get_w2v_models.py
@@ -0,0 +1,40 @@
+import numpy as np
+from mhystic.embedding import *
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+if "uniprot.fasta" in os.listdir("./data/"):
+    pass
+else:
+    print("No Uniprot data. Run : wget http://www.uniprot.org/uniprot/?query=human&format=fasta&compress=yes")
+    quit()
+
+def get_9mers(seqs):
+    mers9 = []
+    for cur_seq in seqs:
+        cur_len = len(cur_seq) - len(cur_seq)%9
+        for i in range(0, cur_len, 9):
+            if "X" in cur_seq[i:i+9]:
+                continue
+            mers9.append(cur_seq[i:i+9])
+    return pd.Series(mers9).apply(fill_spaces)
+
+a = 0
+big_sequence = []
+for seq_record in SeqIO.parse("./data/uniprot.fasta", "fasta"):
+    a+=1
+    if a == 5:
+        break
+    big_sequence.append(str(seq_record.seq))
+
+w2v_sequences = get_9mers(big_sequence)
+
+if "9mers.npy" in os.listdir("./data/"):
+    pass
+else:
+    np.save("./data/9mers.npy", np.array(w2v_sequences))
+
+for i in [10, 50, 80]:
+    w2v_model = train_seq2vec(w2v_sequences, w2v_dim=i)
+    print("\n" + 30*"=" + "\n"+ "TRAINING W2V FOR SIZE {} DONE\n".format(i)+30*"="+"\n")
+    w2v_model.save("./w2v_models/up9mers_size_{}.pkl".format(i))
